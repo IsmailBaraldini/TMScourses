196ed0469c8b40b91b6efc9e483e734569f5547d
Ismail Baraldini <romeo.baraldini@gmail.com>
2022-06-17 17:14:01 +0300

    Work with classes

---
+++ src/com/javacourses/sc/lesson6/CreditСard.java
@@ -0,0 +1,18 @@
+package com.javacourses.sc.lesson6;
+
+public class CreditСard {
+
+    long accountnumber;
+    long currentamount;
+    long accrual;
+
+    public CreditСard() {
+    }
+
+    public CreditСard(long accountnumber, long currentamount, long accrual) {
+
+        this.accountnumber = accountnumber;
+        this.currentamount = currentamount;
+        this.accrual = accrual;
+    }
+}
