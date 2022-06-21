84d49e221482704896487e94786820372d79a951
Ismail Baraldini <romeo.baraldini@gmail.com>
2022-06-21 21:30:17 +0300

    changes

--- src/com/javacourses/sc/lesson6/Main.java
+++ src/com/javacourses/sc/lesson6/Main.java
@@ -3,9 +3,10 @@ package com.javacourses.sc.lesson6;
 public class Main {
     public static void main(String[] args) {
 
-        CreditСard creditCard = new CreditСard();
-        CreditСard creditСard2 = new CreditСard();
-        CreditСard creditСard3 = new CreditСard();
+        CreditСard creditCard = new CreditСard("1111 2222 3333 4444",1000);
+        CreditСard creditСard2 = new CreditСard("2222 4444 5555 7777", 500);
+        CreditСard creditСard3 = new CreditСard("8888 6666 4444 1111", 200);
+
 
         creditCard.addAmount(50);
         creditСard2.addAmount(100);
