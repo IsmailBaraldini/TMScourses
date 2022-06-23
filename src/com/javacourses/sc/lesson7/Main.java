04a351988c211b4584e5e6ff41e4e3cd218be6fa
Ismail <romeo.baraldini@gmail.com>
2022-06-23 13:25:20 +0300

    Changes in 1st task

--- src/com/javacourses/sc/lesson7/Main.java
+++ src/com/javacourses/sc/lesson7/Main.java
@@ -1,15 +1,16 @@
 package com.javacourses.sc.lesson7;
 
-import com.javacourses.sc.lesson7.Circle;
-import com.javacourses.sc.lesson7.Triangle;
-import com.javacourses.sc.lesson7.Rectangle;
-import com.javacourses.sc.lesson7.Figures;
-
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-     Circle areaCircle = new Circle(23);
-     Triangle areaTriangle = new Triangle(12,32,20);
-     Rectangle areaRectangle = new Rectangle(23,42);
+        Circle areaCircle = new Circle(23);
+        System.out.println("The area of the Circle is : " + areaCircle.getArea());
+        System.out.println("The Perimetr of the Circle is : " + areaCircle.getGirth());
+        Triangle areaTriangle = new Triangle(12, 12, 13);
+        System.out.println("The area of the Triangle is : " + areaTriangle.getArea());
+        System.out.println("The perimetr of the Triangle is : " + areaTriangle.getGirth());
+        Rectangle areaRectangle = new Rectangle(23, 42);
+        System.out.println("The area of the Rectangle is : " + areaRectangle.getArea());
+        System.out.println("The perimetr of the Rectangle is : " + areaRectangle.getGirth());
+
     }
 }
